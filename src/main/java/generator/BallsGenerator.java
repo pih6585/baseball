d66fc07aa2af7fbee4952793b408cdc7286130d9
pih6585 <pih6585@naver.com>
2021-09-23 15:33:15 +0900

    refactor : ball generator 적용

--- src/main/java/generator/BallsGenerator.java
+++ src/main/java/generator/BallsGenerator.java
@@ -10,16 +10,13 @@ import model.Balls;
 public class BallsGenerator {
 
 	private static final String CHECK_BALLS_SIZE_ERROR_MESSAGE = "야구게임의 볼은 3자리 입니다.";
-	private static final int HUNDRED_UNIT = 100;
-	private static final int THOUSAND_UNIT = 10;
 	private static final int START_INCLUSIVE = 0;
-	private static final int END_EXCLUSIVE = 3;
 	private static final int BALLS_SIZE = 3;
 	private static final int PLUS_NUMBER = 1;
 
 	public static Balls createTargetBalls() {
 		List<Ball> balls = new ArrayList<>();
-		IntStream.range(START_INCLUSIVE, END_EXCLUSIVE)
+		IntStream.range(START_INCLUSIVE, BALLS_SIZE)
 			.forEach(i -> balls.add(addBall(balls)));
 		return new Balls(balls);
 	}
@@ -39,9 +36,9 @@ public class BallsGenerator {
 
 	private static List<Ball> toList(int ballsNumber) {
 		List<Ball> balls = new ArrayList<>();
-		balls.add(new Ball(ballsNumber / HUNDRED_UNIT));
-		balls.add(new Ball((ballsNumber % HUNDRED_UNIT) / THOUSAND_UNIT));
-		balls.add(new Ball((ballsNumber % HUNDRED_UNIT) % THOUSAND_UNIT));
+		balls.add(new Ball(BallGenerator.generateFirstBall(ballsNumber)));
+		balls.add(new Ball(BallGenerator.generateSecondBall(ballsNumber)));
+		balls.add(new Ball(BallGenerator.generateThirdBall(ballsNumber)));
 		return balls;
 	}
 
