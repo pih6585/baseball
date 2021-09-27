87de5361c63d198b3df65f1194b0cf22db86e717
pih6585 <pih6585@naver.com>
2021-09-27 10:33:56 +0900

    refactor : 게임 재시작 관련 추가에 따른 리팩토링

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -2,26 +2,43 @@ package controller;
 
 import generator.BallsGenerator;
 
+import model.Ball;
 import model.Balls;
 import model.BaseBallGame;
+import model.GameReStart;
 import model.GameResult;
-import view.InputVIew;
+import view.InputView;
 import view.OutputView;
 
 public class Controller {
 
 	public void run() {
-		Balls targetBalls = BallsGenerator.createTargetBalls();
 
+		boolean isGameEnd = gameStart(BallsGenerator.createTargetBalls());
+
+		while (isReGame() && isGameEnd) {
+			isGameEnd = gameStart(BallsGenerator.createTargetBalls());
+		}
+
+		OutputView.printEndGame();
+	}
+
+	private boolean gameStart(Balls targetBalls) {
 		BaseBallGame baseBallGame = new BaseBallGame(targetBalls);
 
-		GameResult gameResult = baseBallGame.play(BallsGenerator.createCustomBalls(InputVIew.inputBalls()));
+		GameResult gameResult = baseBallGame.play(BallsGenerator.createCustomBalls(InputView.inputBalls()));
+
 		while (baseBallGame.isContinue()) {
 			OutputView.printGameResult(gameResult);
-			gameResult = baseBallGame.play(BallsGenerator.createCustomBalls(InputVIew.inputBalls()));
+			gameResult = baseBallGame.play(BallsGenerator.createCustomBalls(InputView.inputBalls()));
 		}
 
 		OutputView.printGameResult(gameResult);
 
+		return !baseBallGame.isContinue();
+	}
+
+	private boolean isReGame() {
+		return new GameReStart(InputView.inputChoiceReGame()).isReGame();
 	}
 }
