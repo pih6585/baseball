0d88190237c8a2f4a308ba0911d7264ed61f04d6
pih6585 <pih6585@naver.com>
2021-09-28 09:51:51 +0900

    refactor : status enum으로 변경

--- src/main/java/model/Status.java
+++ src/main/java/model/Status.java
@@ -1,52 +1,14 @@
 package model;
 
-import java.util.Objects;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
-public class Status {
+public enum Status {
+	STRIKE, BALL, NOTHING;
 
-	private static final int PLUS_NUMBER = 1;
-	private static final int ZERO_POINT = 0;
-
-	private final int strike;
-	private final int ball;
-
-	private Status(int strike, int ball) {
-		this.strike = strike;
-		this.ball = ball;
-	}
-
-	public static Status init() {
-		return new Status(ZERO_POINT, ZERO_POINT);
-	}
-
-	public Status strike() {
-		return new Status(strike + PLUS_NUMBER, ball);
-	}
-
-	public Status ball() {
-		return new Status(strike, ball + PLUS_NUMBER);
-	}
-
-	public int getStrike() {
-		return strike;
-	}
-
-	public int getBall() {
-		return ball;
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		if (this == o)
-			return true;
-		if (o == null || getClass() != o.getClass())
-			return false;
-		Status status = (Status)o;
-		return strike == status.strike && ball == status.ball;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(strike, ball);
+	public static List<Status> getValues() {
+		return Arrays.stream(Status.values())
+			.collect(Collectors.toList());
 	}
 }
