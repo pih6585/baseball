09ddbfc1bf4cb7025a64c423ae640768cad91170
pih6585 <pih6585@naver.com>
2021-10-13 16:29:16 +0900

    feat : scanner util 구성

---
+++ src/main/java/dice/scanner/DiceScanner.java
@@ -0,0 +1,12 @@
+package dice.scanner;
+
+import java.util.Scanner;
+
+public class DiceScanner {
+
+	private static final Scanner scanner = new Scanner(System.in);
+
+	public static String readLine() {
+		return scanner.nextLine();
+	}
+}
