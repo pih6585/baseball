ea3a4a292b0bab4b0cb7853649dba4ac8692e786
pih6585 <pih6585@naver.com>
2021-10-19 10:51:08 +0900

    feat : 참가인원 예외처리 추가

--- src/main/java/dice/model/PlayerNumber.java
+++ src/main/java/dice/model/PlayerNumber.java
@@ -4,6 +4,13 @@ import java.util.Objects;
 
 public class PlayerNumber {
 
+	private static final String PLAYER_NUMBER_EMPTY_OR_NULL_ERROR_MESSAGE = "참가인원이 빈값이거나 null 입니다.";
+	private static final String NUMBER_RANGE_ERROR_MESSAGE = "참가인원은 2~4명 입니다.";
+	private static final String NUMBER_MATCH_TYPE_ERROR_MESSAGE = "숫자만 입력 가능 합니다.";
+	private static final String REGEX = "^\\d+$";
+	private static final int MIN_PLAYER_NUMBER = 2;
+	private static final int MAX_PLAYER_NUMBER = 4;
+
 	private final int playerNumber;
 
 	public PlayerNumber(int playerNumber) {
@@ -11,7 +18,34 @@ public class PlayerNumber {
 	}
 
 	public PlayerNumber(String playerNumber) {
-		this.playerNumber = Integer.parseInt(playerNumber);
+		checkEmptyOrNull(playerNumber);
+		checkMatchType(playerNumber);
+		this.playerNumber = toInt(playerNumber);
+	}
+
+	private int toInt(String playerNumber) {
+		int number = Integer.parseInt(playerNumber);
+		checkRangeNumber(number);
+		return number;
+	}
+
+	private void checkRangeNumber(int number) {
+		if (number < MIN_PLAYER_NUMBER || number > MAX_PLAYER_NUMBER) {
+			throw new IllegalArgumentException(NUMBER_RANGE_ERROR_MESSAGE);
+		}
+	}
+
+	private void checkMatchType(String playerNumber) {
+		if (!playerNumber.matches(REGEX)) {
+			throw new IllegalArgumentException(NUMBER_MATCH_TYPE_ERROR_MESSAGE);
+		}
+	}
+
+	private void checkEmptyOrNull(String playerNumber) {
+		if (playerNumber == null || playerNumber.isEmpty()) {
+			throw new IllegalArgumentException(PLAYER_NUMBER_EMPTY_OR_NULL_ERROR_MESSAGE);
+		}
+
 	}
 
 	@Override
--- src/test/java/dice/model/playerNumberTest.java
+++ src/test/java/dice/model/playerNumberTest.java
@@ -1,18 +1,51 @@
 package dice.model;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class playerNumberTest {
 
 	@Test
 	@DisplayName("참가 인원을 생성한다.")
 	public void createPlayerNumber() {
-		PlayerNumber playerNumber = new PlayerNumber(2);
+		PlayerNumber playerNumber = new PlayerNumber("2");
 
-		assertThat(playerNumber).isEqualTo(new PlayerNumber(2));
+		assertThat(playerNumber).isEqualTo(new PlayerNumber("2"));
+	}
+
+	@Test
+	@DisplayName("참가 인원이 빈값이면 예외가 발생된다.")
+	public void checkEmpty() {
+		assertThrows(IllegalArgumentException.class,
+			() -> new PlayerNumber(""));
+	}
+
+	@Test
+	@DisplayName("참가 인원이 null 이면 예외가 발생된다.")
+	public void checkNull() {
+		assertThrows(IllegalArgumentException.class,
+			() -> new PlayerNumber(null));
+	}
+
+	@ParameterizedTest
+	@DisplayName("참가 인원이 2~4명사이가 아니면 예외가 발생한다.")
+	@CsvSource(value = {"1", "5"})
+	public void checkRangeNumber(String number) {
+		assertThrows(IllegalArgumentException.class,
+			() -> new PlayerNumber(number));
+	}
+
+	@ParameterizedTest
+	@DisplayName("참가 인원은 숫자가 아니면 예외가 발생한다.")
+	@CsvSource(value = {"12!", "qw2", "ㅂㅁ"})
+	public void checkMatchType(String number) {
+		assertThrows(IllegalArgumentException.class,
+			() -> new PlayerNumber(number));
 	}
 
 }
